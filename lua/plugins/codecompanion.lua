5dabd7dd08574f6e51756624cfd0e952b171f82e
edumudu <eduardomudutiu@gmail.com>
2026-02-14 01:26:43 -0300

    lock code companion version to supress warning

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,11 +1,13 @@
 return {
   "olimorris/codecompanion.nvim",
+  tag = "v17.33.0",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
   opts = {
     -- NOTE: The log_level is in `opts.opts`
+    ignore_warnings = true,
     opts = {
       log_level = "TRACE", -- or "TRACE"
     },
