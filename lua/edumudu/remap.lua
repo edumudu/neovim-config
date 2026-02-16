c83210b066644e9815fe4086bf7515b56fb58fbe
edumudu <eduardomudutiu@gmail.com>
2026-02-15 23:00:41 -0300

    feat: add some remapts

--- lua/edumudu/remap.lua
+++ lua/edumudu/remap.lua
@@ -21,3 +21,31 @@ vim.keymap.set("n", "gP", "\"0P")
 vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete without copying to register" })
 vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete without copying to register" })
 
+-- Resize windows continuously
+vim.keymap.set("n", "<C-Up>",    ":resize +2<CR>")
+vim.keymap.set("n", "<C-Down>",  ":resize -2<CR>")
+vim.keymap.set("n", "<C-Left>",  ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Vertical navigation
+
+vim.keymap.set("n", "=", function()
+  if vim.v.count > 0 then
+    return "+"
+  else
+    return "="
+  end
+end, { expr = true })
+
+vim.keymap.set("n", "=", function()
+  if vim.v.count > 0 then
+    return "+"
+  else
+    return "="
+  end
+end, { expr = true })
+
+vim.keymap.set("n", "<leader>k", "kg_", { desc = "Move up to the end of the line" })
+vim.keymap.set("v", "<leader>k", "kg_", { desc = "Move up to the end of the line" })
+vim.keymap.set("n", "<leader>j", "jg_", { desc = "Move down to the end of the line" })
+vim.keymap.set("v", "<leader>j", "jg_", { desc = "Move down to the end of the line" })
