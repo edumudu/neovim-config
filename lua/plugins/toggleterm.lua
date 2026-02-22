f55ce90fe3c4168e6b405bdd63112e77ddfdd7cd
edumudu <eduardomudutiu@gmail.com>
2026-02-22 20:44:41 -0300

    fix: add keybind to open terminal also on normal mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,6 +11,7 @@ return {
     config = function(_, opts)
       require("toggleterm").setup(opts)
 
+      vim.keymap.set("n", "<c-t>", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Open Terminal" })
       vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Float Terminal" })
       -- vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", { desc = "Horizontal Terminal" })
       vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "Vertical Terminal" })
