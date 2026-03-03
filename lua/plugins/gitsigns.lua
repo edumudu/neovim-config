04d8191821faa63bbfde0161b582a89155663352
edumudu <eduardomudutiu@gmail.com>
2026-03-03 00:02:11 -0300

    feat: update configs to be closer to raw cofnig

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    -- We use the `opts` table to merge our changes with AstroNvim's defaults
+    opts = {
+      update_debounce = 100,
+      current_line_blame = true,
+    },
+  },
+}
